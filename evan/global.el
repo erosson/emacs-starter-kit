61ffca67c08b0384a6f43a5681e73e12ec1ef7ae
Evan <evan@scruffy>
2010-09-28 22:22:02 -0700

    fix smart-tab for clojure

--- evan/global.el
+++ evan/global.el
@@ -1,4 +1,5 @@
-(smart-tab-mode)
-
 ; spaces not tabs
 (setq indent-tabs-mode nil)
+
+; just (smart-tab-mode) doesn't work for some reason. Add this to other hooks as needed until we learn how this works.
+(add-hook 'clojure-mode-hook 'smart-tab-mode)
