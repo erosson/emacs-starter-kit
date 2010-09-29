35142806140df398dbaf2a1191a41cd66b2563e5
Evan <evan@scruffy>
2010-09-28 21:07:22 -0700

    colors

---
+++ evan/style.el
@@ -0,0 +1,8 @@
+; http://a-nickels-worth.blogspot.com/2007/11/effective-emacs.html
+(set-background-color "black")
+(set-face-background 'default "black")
+(set-face-background 'region "black")
+(set-face-foreground 'default "white")
+(set-face-foreground 'region "gray60")
+(set-foreground-color "white")
+(set-cursor-color "red")
