1638118dc3bbe0c03a4eaa8bca6934c4a261bbda
Evan <evan@scruffy>
2010-11-08 21:48:55 -0800

    js, coffee-mode

--- evan/global.el
+++ evan/global.el
@@ -6,24 +6,64 @@
 
 (setq default-tab-width 2)
 
-(require 'haxe-mode)
-(defconst my-haxe-style
-  '("java" (c-offsets-alist . ((case-label . +)
-                               (arglist-intro . +)
-                               (arglist-cont-nonempty . 0)
-                               (arglist-close . 0)
-                               (cpp-macro . 0))))
-  "My haXe Programming Style")
-(add-hook 'haxe-mode-hook
-  (function (lambda () (c-add-style "haxe" my-haxe-style t))))
-(add-hook 'haxe-mode-hook
-          (function
-           (lambda ()
-             (setq tab-width 4)
-             (setq indent-tabs-mode t)
-             (setq fill-column 80)
-             (setq c-electric-flag t)
-             (local-set-key [(return)] 'newline-and-indent))))
+(add-to-list 'load-path "~/.emacs.d/evan/coffee-mode")
+(require 'coffee-mode)
 
-(add-hook 'haxe-mode-hook 'smart-tab-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+;(require 'haxe-mode)
+;(defconst my-haxe-style
+;  '("java" (c-offsets-alist . ((case-label . +)
+;                               (arglist-intro . +)
+;                               (arglist-cont-nonempty . 0)
+;                               (arglist-close . 0)
+;                               (cpp-macro . 0))))
+;  "My haXe Programming Style")
+;(add-hook 'haxe-mode-hook
+;  (function (lambda () (c-add-style "haxe" my-haxe-style t))))
+;(add-hook 'haxe-mode-hook
+;          (function
+;           (lambda ()
+;             (setq tab-width 4)
+;             (setq indent-tabs-mode t)
+;             (setq fill-column 80)
+;             (setq c-electric-flag t)
+;             (local-set-key [(return)] 'newline-and-indent))))
+;
+;(add-hook 'haxe-mode-hook 'smart-tab-mode)
 ; (add-hook 'haxe-mode-hook 'paredit-mode)
+
+(require 'flymake)
+
+; http://www.emacswiki.org/emacs/FlymakeJavaScript
+(defun flymake-jslint-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+		      temp-file
+		      (file-name-directory buffer-file-name))))
+    (list "rhino" (list (expand-file-name "~/.emacs.d/evan/jslint.js") local-file))))
+
+(setq flymake-allowed-file-name-masks
+      (cons '(".+\\.js$"
+	      flymake-jslint-init
+	      flymake-simple-cleanup
+	      flymake-get-real-file-name)
+	    flymake-allowed-file-name-masks))
+
+(setq flymake-err-line-patterns 
+      (cons '("^Lint at line \\([[:digit:]]+\\) character \\([[:digit:]]+\\): \\(.+\\)$"  
+	      nil 1 2 3)
+	    flymake-err-line-patterns))
+(add-hook 'espresso-mode-hook (lambda () (flymake-mode t)))
+; match jslint defaults
+(add-hook 'espresso-mode-hook (lambda ()
+                                (setq default-tab-width 4)
+                                (setq espresso-indent-level 4)))
+
+(global-set-key (kbd "C-c n") 'flymake-display-err-menu-for-current-line) 
--- starter-kit-js.el
+++ starter-kit-js.el
@@ -6,7 +6,8 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (add-hook 'espresso-mode-hook 'moz-minor-mode)
-(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
+; http://github.com/technomancy/emacs-starter-kit/issues#issue/7
+;(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
 (setq espresso-indent-level 2)
 
@@ -14,15 +15,17 @@
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 
 (eval-after-load 'espresso
-  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
-          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
-          ;; fixes problem with pretty function font-lock
+  '(progn ;(define-key espresso-mode-map "{" 'paredit-open-curly)
+          ;(define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
+          ; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-          (font-lock-add-keywords
-           'espresso-mode `(("\\(function *\\)("
-                             (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
-                                       nil)))))))
+          ; http://github.com/technomancy/emacs-starter-kit/issues#issue/7
+          ;(font-lock-add-keywords
+          ; 'espresso-mode `(("\\(function *\\)("
+          ;                   (0 (progn (compose-region (match-beginning 1)
+          ;                                             (match-end 1) "ƒ")
+          ;                             nil)))))
+                                       ))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
